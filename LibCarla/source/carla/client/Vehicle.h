03a60a48d2d1412ee67a34ab20be58e4acccb9a5
Jacopo Bartiromo <jackbart94@gmail.com>
2020-06-04 12:02:07 +0200

    Removed import and placed constant

--- LibCarla/source/carla/client/Vehicle.h
+++ LibCarla/source/carla/client/Vehicle.h
@@ -11,8 +11,8 @@
 #include "carla/rpc/VehicleLightState.h"
 #include "carla/rpc/VehicleControl.h"
 #include "carla/rpc/VehiclePhysicsControl.h"
-#include "carla/trafficmanager/Constants.h"
 
+#define TM_PORT 8000
 
 namespace carla {
 
@@ -37,7 +37,7 @@ namespace client {
     using ActorState::GetBoundingBox;
 
     /// Switch on/off this vehicle's autopilot.
-    void SetAutopilot(bool enabled = true, uint16_t tm_port = traffic_manager::constants::Networking::TM_DEFAULT_PORT);
+    void SetAutopilot(bool enabled = true, uint16_t tm_port = TM_PORT);
 
     /// Apply @a control to this vehicle.
     void ApplyControl(const Control &control);
